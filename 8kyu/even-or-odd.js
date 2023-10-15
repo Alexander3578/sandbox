876c3adfac3d9f7451abf8170fc5f498e0f28155
Alexander Stasyuk <predator12007@mail.ru>
2023-10-15 16:19:57 +0300

    new codewarsTask

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+//     Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// 👌 SOLUTION:
+function evenOrOdd(number) {
+    return (number % 2 === 0 ? 'Even' : 'Odd')
+}
